515f7db40de357675385614db0e0023989f3f565
TAHMINA NOORZAY <tahminanoorzay1380@gmail.com>
2025-01-25 19:32:51 +0430

    delete another files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import reportWebVitals from "./reportWebVitals";
 
 function App() {
   return (
@@ -20,5 +19,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-reportWebVitals();
