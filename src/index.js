6c6f1813021c715f0524aee638fcb4311e6920a1
TAHMINA NOORZAY <tahminanoorzay1380@gmail.com>
2025-01-25 19:22:19 +0430

    delete some files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+
+function App() {
+  return (
+    <div>
+      <img
+        src="/first-react-app/src/img/WhatsApp Image 2024-11-19 at 08.41.17_81a82593.jpg"
+        alt=""
+      />
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
