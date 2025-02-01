af2ba489709845bf34724270cad813eec92daa17
TAHMINA NOORZAY <tahminanoorzay1380@gmail.com>
2025-02-01 15:39:47 +0430

    add ( + and - ) buttons add date

--- src/index.js
+++ src/index.js
@@ -2,13 +2,35 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
+let counter = 0;
+let date = new Date();
+
+function handleIncrease() {
+  counter++;
+  date.setDate(date.getDate() + 1);
+  updateUI();
+}
+
+function handleDecrase() {
+  counter--;
+  date.setDate(date.getDate() - 1);
+  updateUI();
+}
+
+function updateUI() {
+  document.getElementById("counter").innerHTML = `Counter : ${counter}`;
+  document.getElementById(
+    "day"
+  ).innerHTML = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}, ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} `;
+}
+
 function App() {
   return (
     <div>
-      <img
-        src="/first-react-app/src/img/WhatsApp Image 2024-11-19 at 08.41.17_81a82593.jpg"
-        alt=""
-      />
+      <h1 id="counter">{`Counter : ${counter}`}</h1>
+      <button onClick={handleIncrease}>+</button>
+      <button onClick={handleDecrase}>-</button>
+      <div id="day">{`${date.getDate()}/${date.getMonth()}/${date.getFullYear()}, ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} AM`}</div>
     </div>
   );
 }
